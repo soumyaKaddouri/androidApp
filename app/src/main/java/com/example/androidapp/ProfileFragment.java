f00486ab254922e6910ff84092795e682dc15308
Soumya Kaddouri <75394110+soumyaKaddouri@users.noreply.github.com>
2022-05-01 23:28:36 +0000

    finshing ProfileFragment.java

--- app/src/main/java/com/example/androidapp/ProfileFragment.java
+++ app/src/main/java/com/example/androidapp/ProfileFragment.java
@@ -1,64 +1,93 @@
 package com.example.androidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ProfileFragment extends Fragment {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    TextView fName, fNameUser, phoneNumber, email, type;
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        fNameUser = getActivity().findViewById(R.id.fNameUser);
+        fName = getActivity().findViewById(R.id.fName);
+        type = getActivity().findViewById(R.id.userType);
+        phoneNumber = getActivity().findViewById(R.id.textViewPhone);
+        email = getActivity().findViewById(R.id.textViewEmail);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        DocumentReference documentReference;
+        documentReference = db.collection("users").document(user.getEmail());
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.getResult().exists()){
+                    String nameRes = task.getResult().getString("fName");
+                    String emailRes = task.getResult().getString("email");
+                    String phoneRes = task.getResult().getString("phoneNum");
+                    String typeRes = task.getResult().getString("type");
+
+                    fName.setText(nameRes);
+                    fNameUser.setText(nameRes);
+                    email.setText(emailRes);
+                    phoneNumber.setText(phoneRes);
+                    type.setText(typeRes);
+
+                }else{
+                    Intent intent = new Intent(getActivity(), Home.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
