9137dedb816ab07ecde5fb945d9b2e317460479c
Soumya Kaddouri <75394110+soumyaKaddouri@users.noreply.github.com>
2022-05-02 00:11:39 +0000

    fixing some navigation header bar probs

--- app/src/main/java/com/example/androidapp/Home.java
+++ app/src/main/java/com/example/androidapp/Home.java
@@ -17,13 +17,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Home extends AppCompatActivity {
-    Button btnPrf, btnStd, btnLogOut;
+    TextView fName, type;
     FirebaseAuth mAuth;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,36 @@ public class Home extends AppCompatActivity {
 
         NavigationView navigationView = findViewById(R.id.navigationView);
         navigationView.setItemIconTintList(null);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        DocumentReference documentReference;
+        documentReference = db.collection("users").document(user.getEmail());
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.getResult().exists()){
+                    fName = navigationView.findViewById(R.id.fullName);
+                    type = navigationView.findViewById(R.id.type);
+                    String nameRes = task.getResult().getString("fName");
+                    String typeRes = task.getResult().getString("type");
+                    System.out.println(nameRes);
+                    System.out.println(typeRes);
+                    System.out.println(fName);
+                    fName.setText(nameRes);
+                    type.setText(typeRes);
+
+
+                }else{
+                    Intent intent = new Intent(Home.this, ProfileFragment.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
         NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
         NavigationUI.setupWithNavController(navigationView, navController);
